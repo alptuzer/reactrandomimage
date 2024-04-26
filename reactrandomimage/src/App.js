fd00d8143d9c372adf020c26cd09a24cb683f0cc
alp <alptuzer@gmail.com>
2024-04-26 19:35:13 +0300

    1

---
+++ reactrandomimage/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './components/Main'
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
